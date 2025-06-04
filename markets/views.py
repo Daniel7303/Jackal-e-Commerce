5705b543dea0de6a2879b4cd684ef162a66630bd
Daniel7303 <danieleistien@gmail.com>
2025-06-04 03:38:56 +0100

    added a wishlist_count

--- markets/views.py
+++ markets/views.py
@@ -114,9 +114,9 @@ def view_wishlist(request):
     return render(request, 'markets/wishlist.html', {'wishlists':wishlists})
 
 
-# def wishlist_count(request):
-#     if request.user.is_authenticated:
-#         count = Wishlist.objects.filter(user=request.user).count()
-#     else:
-#         count = 0
-#     return {'wishlist_count': count}
+def wishlist_count(request):
+    if request.user.is_authenticated:
+        count = Wishlist.objects.filter(user=request.user).count()
+    else:
+        count = 0
+    return {'wishlist_count': count}
